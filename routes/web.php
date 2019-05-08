bf0f116acaedb5265c12c99c2c095228bddb8097
Davide Casiraghi <davide.casiraghi@gmail.com>
2019-05-08 21:18:21 +0200

    added routes

---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+    Route::group(['namespace' => 'DavideCasiraghi\LaravelSmartBlog\Http\Controllers', 'middleware' => 'web'], function () {
+        
+        /* Post Categories */
+        Route::resource('categories', 'CategoryController');
+
+        /* Category Translations */
+        Route::get('/categoryTranslations/{categoryId}/{languageCode}/create', 'CategoryTranslationController@create');
+        Route::get('/categoryTranslations/{categoryId}/{languageCode}/edit', 'CategoryTranslationController@edit');
+        Route::post('/categoryTranslations/store', 'CategoryTranslationController@store')->name('categoryTranslations.store');
+        Route::put('/categoryTranslations/update', 'CategoryTranslationController@update')->name('categoryTranslations.update');
+        Route::delete('/categoryTranslations/destroy/{categoryTranslationId}', 'CategoryTranslationController@destroy')->name('categoryTranslations.destroy');
+
+        /* Posts */
+        Route::resource('posts', 'PostController');
+        Route::get('/post/{slug}', 'PostController@postBySlug')->where('postBySlug', '[a-z]+');
+
+        /* Posts Translations */
+        Route::get('/postTranslations/{postId}/{languageCode}/create', 'PostTranslationController@create');
+        Route::get('/postTranslations/{postId}/{languageCode}/edit', 'PostTranslationController@edit');
+        Route::post('/postTranslations/store', 'PostTranslationController@store')->name('postTranslations.store');
+        Route::put('/postTranslations/update', 'PostTranslationController@update')->name('postTranslations.update');
+        Route::delete('/postTranslations/destroy/{postTranslationId}', 'PostTranslationController@destroy')->name('postTranslations.destroy');
+
+    });
+
+    
