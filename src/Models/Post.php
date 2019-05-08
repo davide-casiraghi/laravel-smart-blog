da839b28beda30b3952d076a55dcf102b15e9453
Davide Casiraghi <davide.casiraghi@gmail.com>
2019-05-08 23:24:39 +0200

    added post model

---
+++ src/Models/Post.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace DavideCasiraghi\LaravelSmartBlog\Models;
+
+use Dimsav\Translatable\Translatable;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    /***************************************************************************/
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'posts';
+
+    /***************************************************************************/
+
+    use Translatable;
+
+    public $translatedAttributes = ['title', 'body', 'slug', 'before_content', 'after_content', 'extra_field_trans_1', 'extra_field_trans_2'];
+    protected $fillable = ['author_id', 'category_id', 'meta_description', 'meta_keywords', 'seo_title', 'image', 'status', 'featured', 'introimage_src', 'introimage_alt', 'extra_field_1', 'extra_field_2', 'extra_field_3'];
+
+    /***************************************************************************/
+
+    /**
+     * Return all the posts by category id in the language specified.
+     *
+     * @param  int $cat_id
+     * @return \App\Post
+     */
+    public static function postsByCategory($cat_id)
+    {
+        $ret = self::
+               where('category_id', $cat_id)
+               ->get();
+
+        return $ret;
+    }
+}
---
+++ src/Models/PostTranslation.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace DavideCasiraghi\LaravelSmartBlog\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class PostTranslation extends Model
+{
+    /***************************************************************************/
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'post_translations';
+
+    /***************************************************************************/
+
+    public $timestamps = false;
+    protected $fillable = ['title', 'body', 'slug', 'before_content', 'after_content', 'extra_field_trans_1', 'extra_field_trans_2'];
+}
