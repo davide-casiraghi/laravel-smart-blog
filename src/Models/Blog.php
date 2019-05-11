131a5dc258c519ef636ae8412a8f233d6b54a21b
Davide Casiraghi <davide.casiraghi@gmail.com>
2019-05-11 13:07:58 +0200

    created blog model

---
+++ src/Models/Blog.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace DavideCasiraghi\LaravelSmartBlog\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Blog extends Model
+{
+    /***************************************************************************/
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'blogs';
+
+    /***************************************************************************/
+
+    protected $fillable = [
+        'category_id', 
+        'layout', 
+        'columns',
+        'article_order',
+        'pagination',
+        'featured_articles',
+        'show_category_title',
+        'show_category_subtitle',
+        'show_category_description',
+        'show_category_image',
+        'show_post_title',
+        'post_linked_titles',
+        'show_post_intro_text',
+        'show_post_author',
+        'link_post_author',
+        'show_create_date',
+        'show_modify_date',
+        'show_publish_date',
+        'show_read_more',
+        'created_by',
+    ];
+
+    /***************************************************************************/
+
+
+}
