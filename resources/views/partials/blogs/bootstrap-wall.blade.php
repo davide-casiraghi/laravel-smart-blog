f2ef22203ec7738ab8dd76fe14a7df4a3112823f
Davide Casiraghi <davide.casiraghi@gmail.com>
2019-05-11 15:46:25 +0200

    adding blog show partials

--- resources/views/partials/blogs/bootstrap-wall.blade.php
+++ resources/views/partials/blogs/bootstrap-wall.blade.php
@@ -1,6 +1,26 @@
 aaa
 
-
-@foreach ($posts as $key => $post)
+<div class="row">
     
+@foreach ($posts as $key => $post)
+    <div class="col-12">
+        <h4>{{ $post->title }}</h4>
+    </div>
+    <div class="col-12">
+        @if(!empty($post->image))
+            <img class="ml-sm-3 float-sm-right img-fluid" alt="{{ $post->title }}" src="/storage/images/events_teaser/thumb_{{ $post->image }}">
+        @else
+            <div class="alert alert-danger" role="alert">
+                Image not found
+            </div>
+        @endif
+    </div>
+    
+    
+    
+    {{--<div class="col-12">
+        {!! $event->title !!}
+    </div>--}}
 @endforeach
+
+</div>
