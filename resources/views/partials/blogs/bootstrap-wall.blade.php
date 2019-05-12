6a778b3b9e123ff0063641acda24c3cafdee3852
Davide Casiraghi <davide.casiraghi@gmail.com>
2019-05-12 16:56:19 +0200

    fixed post image

--- resources/views/partials/blogs/bootstrap-wall.blade.php
+++ resources/views/partials/blogs/bootstrap-wall.blade.php
@@ -6,8 +6,8 @@
         <div class="col-12 {{$columnsBootstrapClass}}">
             <h4>{{ $post->title }}</h4>
         
-            @if(!empty($post->image))
-                <img class="ml-sm-3 float-sm-right img-fluid" alt="{{ $post->title }}" src="/storage/images/posts_intro_images/thumb_{{ $post->image }}">
+            @if(!empty($post->introimage))
+                <img class="img-fluid" alt="{{ $post->title }}" src="/storage/images/posts_intro_images/thumb_{{ $post->introimage }}">
             @else
                 <div class="alert alert-warning" role="alert">
                     Image not found
