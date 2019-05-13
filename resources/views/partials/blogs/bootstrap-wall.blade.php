b3a5f731a8dc8b2fe010bf8d9b95837a4d1511bb
Davide Casiraghi <davide.casiraghi@gmail.com>
2019-05-13 16:16:50 +0200

    fixed post_linked_titles

--- resources/views/partials/blogs/bootstrap-wall.blade.php
+++ resources/views/partials/blogs/bootstrap-wall.blade.php
@@ -4,7 +4,15 @@
         
     @foreach ($posts as $key => $post)
         <div class="col-12 col-sm-6 {{$columnsBootstrapClass}} mb-5">
-            <h4>{{ $post->title }}</h4>
+            @if(!empty($blog->show_post_title))
+                @if(!empty($blog->post_linked_titles))
+                    <h4><a href="{{ route('posts.show',$post->id) }}">{{ $post->title }}</a></h4>
+                @else
+                    <h4>{{ $post->title }}</h4>
+                @endif
+            @endif
+            
+            
         
             @if(!empty($post->introimage))
                 <img class="img-fluid" alt="{{ $post->title }}" src="/storage/images/posts_intro_images/thumb_{{ $post->introimage }}">
