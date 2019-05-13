b3a564015f80924a44944f5196f7f99bfadbec1d
Davide Casiraghi <davide.casiraghi@gmail.com>
2019-05-13 16:28:57 +0200

    Apply fixes from StyleCI (#15)

--- src/Models/Post.php
+++ src/Models/Post.php
@@ -24,7 +24,7 @@ class Post extends Model
 
     /***************************************************************************/
 
-    /**
+    /*
      * Return all the posts by category id in the language specified.
      *
      * @param  int $cat_id
