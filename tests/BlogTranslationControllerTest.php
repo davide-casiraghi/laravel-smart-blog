36e91ffe315edd0dcaeb8d23a0da855c34ff502b
Davide Casiraghi <davide.casiraghi@gmail.com>
2019-05-13 08:26:28 +0200

    testing blog translations

--- tests/BlogTranslationControllerTest.php
+++ tests/BlogTranslationControllerTest.php
@@ -168,12 +168,31 @@ class BlogTranslationControllerTest extends TestCase
     }
 
     /** @test */
-    /*public function it_does_not_update_invalid_blog()
+    public function it_does_not_update_invalid_blog()
     {
         $this->authenticateAsAdmin();
         $blog = factory(Blog::class)->create([
-                            'name' => 'Regular Jams',
-                            'slug' => 'regular-jams',
+                'category_id' => '1',
+                'layout' => '1',
+                'columns_number' => '3',
+                'columns_width' => '200',
+                'article_order' => '1',
+                'items_per_page' => '20',
+                'featured_articles' => '1',
+                'show_category_title' => '1',
+                'show_category_subtitle' => '1',
+                'show_category_description' => '1',
+                'show_category_image' => '1',
+                'show_post_title' => '1',
+                'post_linked_titles' => '1',
+                'show_post_intro_text' => '1',
+                'show_post_author' => '1',
+                'link_post_author' => '1',
+                'show_create_date' => '1',
+                'show_modify_date' => '1',
+                'show_publish_date' => '1',
+                'show_read_more' => '1',
+                'created_by' => '1',
                         ]);
 
         $data = [
@@ -193,10 +212,10 @@ class BlogTranslationControllerTest extends TestCase
         $response = $this->followingRedirects()
                          ->put('/blogTranslations/update', $attributes);
         $response->assertSessionHasErrors();
-    }*/
+    }
 
     /** @test */
-    /*public function it_deletes_blog_translation()
+    public function it_deletes_blog_translation()
     {
         $this->authenticateAsAdmin();
         $blog = factory(Blog::class)->create();
@@ -211,5 +230,5 @@ class BlogTranslationControllerTest extends TestCase
 
         $response = $this->delete('/blogTranslations/destroy/2');
         $response->assertRedirect('/blogs');
-    }*/
+    }
 }
